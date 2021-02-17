809a05f6b6d4628f9c5cb2f8628b9c897ee1cab9
Cylindric <mark@hanfordonline.co.uk>
2021-02-17 14:16:49 +0000

    Fix inconsistent filename capitalisation
    It's possible for the various paths to be set to be fully case-sensitive now, so SDL2 won't be found if it's called SDL2.dll and needs to be SDL2.DLL

--- assets/generate.bat
+++ assets/generate.bat
@@ -5,7 +5,7 @@ SET BUILDROOT=%1
 SET BUILDROOT=%BUILDROOT:"=%
 
 SET TP="C:\Program Files\CodeAndWeb\TexturePacker\bin\TexturePacker.exe"
-SET BASEOUTPUTPATH=%BUILDROOT%\assets\base
+SET BASEOUTPUTPATH=%BUILDROOT%assets\base
 SET OPTIONS=--format xml --trim-sprite-names --extrude 0 --algorithm Basic 
 SET OPTIONS=%OPTIONS% --trim-mode None --png-opt-level 0 --disable-auto-alias 
 SET OPTIONS=%OPTIONS% --disable-rotation --size-constraints POT --force-word-aligned
@@ -41,17 +41,17 @@ ECHO # Images
 ECHO ###########################################################################
 
 SET OUTPUTPATH=%BASEOUTPUTPATH%\images\characters
-%TP% %OPTIONS% --data %OUTPUTPATH%\colonist.xml --sheet %OUTPUTPATH%\colonist.png characters\colonist-assets
+%TP% %OPTIONS% --data %OUTPUTPATH%\colonist.xml --sheet %OUTPUTPATH%\colonist.png Characters\colonist-assets
 
 SET OUTPUTPATH=%BASEOUTPUTPATH%\images\cursors
 %TP% %OPTIONS% --data %OUTPUTPATH%\cursors.xml --sheet %OUTPUTPATH%\cursors.png Cursors\cursor-assets
 
 SET OUTPUTPATH=%BASEOUTPUTPATH%\images\furniture
-%TP% %OPTIONS% --data %OUTPUTPATH%\furn_wall_steel.xml --sheet %OUTPUTPATH%\furn_wall_steel.png furniture\furn_wall_steel-assets
-%TP% %OPTIONS% --data %OUTPUTPATH%\furn_door_heavy.xml --sheet %OUTPUTPATH%\furn_door_heavy.png furniture\furn_door_heavy-assets
-%TP% %OPTIONS% --data %OUTPUTPATH%\furn_door.xml --sheet %OUTPUTPATH%\furn_door.png furniture\furn_door-assets
-%TP% %OPTIONS% --data %OUTPUTPATH%\furn_oxygen.xml --sheet %OUTPUTPATH%\furn_oxygen.png furniture\furn_oxygen-assets
-%TP% %OPTIONS% --data %OUTPUTPATH%\furn_mining_station.xml --sheet %OUTPUTPATH%\furn_mining_station.png furniture\furn_mining_station-assets
+%TP% %OPTIONS% --data %OUTPUTPATH%\furn_wall_steel.xml --sheet %OUTPUTPATH%\furn_wall_steel.png Furniture\furn_wall_steel-assets
+%TP% %OPTIONS% --data %OUTPUTPATH%\furn_door_heavy.xml --sheet %OUTPUTPATH%\furn_door_heavy.png Furniture\furn_door_heavy-assets
+%TP% %OPTIONS% --data %OUTPUTPATH%\furn_door.xml --sheet %OUTPUTPATH%\furn_door.png Furniture\furn_door-assets
+%TP% %OPTIONS% --data %OUTPUTPATH%\furn_oxygen.xml --sheet %OUTPUTPATH%\furn_oxygen.png Furniture\furn_oxygen-assets
+%TP% %OPTIONS% --data %OUTPUTPATH%\furn_mining_station.xml --sheet %OUTPUTPATH%\furn_mining_station.png Furniture\furn_mining_station-assets
 
 SET OUTPUTPATH=%BASEOUTPUTPATH%\images\backgrounds
 IF NOT EXIST "%OUTPUTPATH%" MD %OUTPUTPATH%
